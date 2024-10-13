5535e36b9ee813fed72a80eb5d4bf0b228049117
Lokesh Keyan <lokeshkeyan@microsoft.com>
2024-10-13 15:47:38 -0400

    error interface and stringer

--- Interfaces/Interfaces.go
+++ Interfaces/Interfaces.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 	//"math"
 )
 
@@ -41,15 +42,70 @@ func main(){
 	// i.M()
 
 	//empty interface
-	var j interface{}
-	describe(j)
+	// var j interface{}
+	// describe(j)
 
-	j = 42
-	describe(j)
+	// j = 42
+	// describe(j)
 
-	j = "hello"
-	describe(j)
+	// j = "hello"
+	// describe(j)
+
+	// // Type Assertions
+	// var i interface{} = "hello"
+	// s := i.(string)
+	// fmt.Println(s)
+
+	// s, ok := i.(string)
+	// fmt.Println(s, ok)
+
+	// f, ok := i.(float64)
+	// fmt.Println(f, ok)
+
+	// this will error out but not the top one
+	// f = i.(float64) // panic
+	// fmt.Println(f)
+
+	//Stringer
+	// hosts := map[string]IPAddr{
+	// 	"loopback":  {127, 0, 0, 1},
+	// 	"googleDNS": {8, 8, 8, 8},
+	// }
+	// for name, ip := range hosts {
+	// 	fmt.Printf("%s: %s\n", name, ip)
+	// }
+
+	//error interface
+	err := &MyError{
+		time.Now(),
+		"it didn't work",
+	}
+
+	if err != nil {
+		fmt.Println(err)
+	}
 }
+
+func (ip IPAddr) String() string{
+	return fmt.Sprintf("%v.%v.%v.%v", ip[0], ip[1], ip[2], ip[3])
+}
+
+type IPAddr [4]byte
+
+type MyError struct{
+	When time.Time
+	What string
+}
+
+func (e * MyError) Error() string{
+	return fmt.Sprintf("at %v, %s",
+		e.When, e.What)
+}
+
+// this is equal to error = &MyError{}
+// func run() error{
+
+// }
 
 // func describe(i I){
 // 	fmt.Printf("(%v, %T)\n", i, i)
