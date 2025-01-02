9b01ddf3f10dec6da9c5390c6d4fca0cd8d0752d
Lokesh Keyan <lokeshkeyanuf@gmail.com>
2025-01-02 16:37:29 -0500

    Leetcode problem - Contains deilicate

---
+++ leetcode/arrayAndHashing/ContainsDuplicate.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func containsDuplicate(nums []int) bool{
+	m := make(map[int]bool)
+	for _, num := range nums {
+		if m[num] {
+			return true
+		}
+		m[num] = true
+	}
+
+	return false
+}
