ac8d6859c2cf9028dc8a68c765dc8e0ded3ff641
Lokesh Keyan <lokeshkeyanuf@gmail.com>
2025-01-03 14:00:28 -0500

    Edit formatting

--- leetcode/arrayAndHashing/ValidAnagram.go
+++ leetcode/arrayAndHashing/ValidAnagram.go
@@ -1,29 +1,29 @@
 package leetcode
 
 func isAnagram(s string, t string) bool {
-    // if length of s and t are different the return false
-    if len(s) != len(t){
-        return false
-    }
+	// if length of s and t are different the return false
+	if len(s) != len(t) {
+		return false
+	}
 
-    charMap := make(map[rune]int)
-        for _, c := range s {
-        charMap[c]++
-    }
+	charMap := make(map[rune]int)
+	for _, c := range s {
+		charMap[c]++
+	}
 
-    for _, c := range t{
-        charMap[c]--
+	for _, c := range t {
+		charMap[c]--
 
-        if charMap[c] < 0{
-            return false
-        }
-    }
+		if charMap[c] < 0 {
+			return false
+		}
+	}
 
-    for _, count := range charMap {
-        if count != 0 {
-            return false
-        }
-    }
+	for _, count := range charMap {
+		if count != 0 {
+			return false
+		}
+	}
 
-    return true
+	return true
 }
