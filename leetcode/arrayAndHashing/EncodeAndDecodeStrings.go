6ede594682cd8a499b4c26e38367d8d3f9d9dcf0
Lokesh Keyan <lokeshkeyanuf@gmail.com>
2025-01-10 14:23:52 -0500

    Encode and Decode string problem

---
+++ leetcode/arrayAndHashing/EncodeAndDecodeStrings.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import "strings"
+
+type Codec struct {
+}
+
+func (codec *Codec) encode(strs []string) string {
+	res := strings.Builder{}
+	for _, str := range strs {
+		res.WriteByte(byte(len(str)))
+		res.WriteString(str)
+	}
+
+	return res.String()
+}
+
+func (codec *Codec) decode(strs string) []string{
+	var count int
+	var data string
+	var ans []string
+	for i := 0; i < len(strs); i += count + 1 {
+		count = int(strs[i])
+		data = strs[i+1:i+1+count]
+		ans = append(ans, data)
+	}
+
+	return ans
+}
